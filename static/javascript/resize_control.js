577064c268dbdc175678c102ffa9203c764ec510
VitorFigm <vitorfigmarques@hotmail.com>
2020-09-14 10:54:08 -0300

    front-ed update

--- static/javascript/resize_control.js
+++ static/javascript/resize_control.js
@@ -1,7 +1,12 @@
+window.higher_res = window.innerWidth > 700
+
 window.onresize = ()=>{
-    if(this.innerWidth>=800){
-        show_hide_anim({search:'moving_in2',search_translucid_screen:'fade_out'})
-    }else{
-        show_hide_anim({search:'moving_in',search_translucid_screen:'fade_in98'})
+    if(this.innerWidth>700 && !window.higher_res  ){
+        show_hide_anim({search:'moving_in2',header:'moving_in2',search_translucid_screen:'fade_out98'})
     }
+    if(this.innerWidth<700 && window.higher_res){
+        show_hide_anim({search:'moving_out2',header:'moving_in2',search_translucid_screen:'fade_out98'})
+    }
+
+    window.higher_res = window.innerWidth > 700
 }
